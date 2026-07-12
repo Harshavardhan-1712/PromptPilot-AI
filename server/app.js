575b5d8108cc99e2594e7d8be703824f247f53a3
BS HARSHA VARDHAN <hunnyharsha@gmail.com>
2026-07-12 20:17:56 +0530

    Change trust proxy setting to boolean true

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ import { notFoundHandler, errorHandler } from './middleware/errorHandler.js';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app = express();
-app.set("trust proxy", 1);
+app.set("trust proxy", true);
 
 app.use(
   cors({
